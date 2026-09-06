72a1fd8425bfc9e657cb53dd235b60cabba98d78
muhammadtaqi512q-oss <muhammadtaqi512q@gmail.com>
2026-09-06 17:46:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ MAIN_HTML = """<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nexura</title>
+    <title>JoyMix</title>
 </head>
 <body style="background-color: #090014; margin: 0;">
 
